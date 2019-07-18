75a99a9847371caa8e51298a1b4a6add106fc605
Bilal Khan <bk@tinymanager.com>
2019-07-18 17:11:34 -0500

    Bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytorch_zoo",
-    version="1.1.1",
+    version="1.1.2",
     author="Bilal Khan",
     author_email="bk@tinymanager.com",
     description="A collection of useful modules and utilities for kaggle not available in Pytorch",
